fd0f858bab449c1263fa3772d66820f7f12e0b3a
Robert Minahan <minahanr75@gmail.com>
2020-08-13 20:33:25 -0700

    updated to parse new structure

--- utils/parseArray.js
+++ utils/parseArray.js
@@ -3,21 +3,28 @@ export default function parseArray(imageIndex) {
     const format = imageSeries[imageIndex].format;
     const numFrames = imageSeries[imageIndex].numFrames
     const imgsPerFrame = imageSeries[imageIndex].imgsPerFrame
-    const startingTimeIndex = 9;
+    const startingTimeIndex = 1;
     const startingSpaceIndex = 1;
     let urlsOverTime = [];
     
-    if (numFrames !== 1) {
-        for (let i = 0; i < numFrames; i++) {
-            urlsOverTime.push([]);
-            for (let j = 0; j < imgsPerFrame; j++) {
-                urlsOverTime[i].push(baseURL + '/' + '0'.repeat(1 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '/' + 'IM-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(1, i * imgsPerFrame + j + 1)))) + (i * imgsPerFrame + j + 1) + '.' + format + '?raw=true');
-            }
-        }
-    } else {
-        urlsOverTime.push([]);
-        for(let i = 0; i < imgsPerFrame; i++) {
-            urlsOverTime[0].push(baseURL + '/' + (1) + '-' + '0'.repeat((Math.floor(Math.log10(imgsPerFrame))) - Math.floor(Math.log10(Math.max(i + startingSpaceIndex, 1)))) + (i + startingSpaceIndex) + '.' + format + '?raw=true');
+    // if (numFrames !== 1) {
+    //     for (let i = 0; i < numFrames; i++) {
+    //         urlsOverTime.push([]);
+    //         for (let j = 0; j < imgsPerFrame; j++) {
+    //             urlsOverTime[i].push(baseURL + '/' + '0'.repeat(1 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '/' + 'IM-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(1, i * imgsPerFrame + j + 1)))) + (i * imgsPerFrame + j + 1) + '.' + format + '?raw=true');
+    //         }
+    //     }
+    // } else {
+    //     urlsOverTime.push([]);
+    //     for(let i = 0; i < imgsPerFrame; i++) {
+    //         urlsOverTime[0].push(baseURL + '/' + (1) + '-' + '0'.repeat((Math.floor(Math.log10(imgsPerFrame))) - Math.floor(Math.log10(Math.max(i + startingSpaceIndex, 1)))) + (i + startingSpaceIndex) + '.' + format + '?raw=true');
+    //     }
+    // }
+
+    urlsOverTime.push([]);
+    for (let i = 0; i < numFrames; i++) {
+        for(let j = 0; j < imgsPerFrame; j++) {
+            urlsOverTime[0].push(baseURL + '/' + '0'.repeat(Math.floor(Math.log10(numFrames)) - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '/' + '0'.repeat(Math.floor(Math.log10(imgsPerFrame)) - Math.floor(Math.log10(Math.max(i + startingSpaceIndex, 1)))) + (i + startingSpaceIndex));
         }
     }
 
