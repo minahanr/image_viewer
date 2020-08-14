612a9ea1e45f351e43cdcf9c381ea853c70ad6bc
Robert Minahan <minahanr75@gmail.com>
2020-08-13 20:41:46 -0700

    more fixes

--- utils/parseArray.js
+++ utils/parseArray.js
@@ -24,7 +24,7 @@ export default function parseArray(imageIndex) {
     for (let i = 0; i < numFrames; i++) {
         urlsOverTime.push([]);
         for(let j = 0; j < imgsPerFrame; j++) {
-            urlsOverTime[i].push(baseURL + '/' + '0'.repeat(Math.floor(Math.log10(numFrames)) - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '-' + '0'.repeat(Math.floor(Math.log10(imgsPerFrame)) - Math.floor(Math.log10(Math.max(i + startingSpaceIndex, 1)))) + (i + startingSpaceIndex) + '.' + format);
+            urlsOverTime[i].push(baseURL + '/' + '0'.repeat(Math.floor(Math.log10(numFrames)) - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '-' + '0'.repeat(Math.floor(Math.log10(imgsPerFrame)) - Math.floor(Math.log10(Math.max(j + startingSpaceIndex, 1)))) + (j + startingSpaceIndex) + '.' + format);
         }
     }
 
