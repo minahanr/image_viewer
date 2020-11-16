57c066c6a21533106c198ca7fb6c1655f6e17e3b
minahanr <minahanr75@gmail.com>
2020-11-16 12:01:19 -0500

    Deleted comments

--- utils/parseArray.js
+++ utils/parseArray.js
@@ -4,27 +4,12 @@ export default function parseArray(imageIndex, projection) {
     const numFrames = imageSeries[imageIndex].numFrames
     const imgsPerFrame = imageSeries[imageIndex].imgsPerFrame
     const startingTimeIndex = 1;
-    const startingSpaceIndex = 1;
     let urlsOverTime = [];
-    
-    // if (numFrames !== 1) {
-    //     for (let i = 0; i < numFrames; i++) {
-    //         urlsOverTime.push([]);
-    //         for (let j = 0; j < imgsPerFrame; j++) {
-    //             urlsOverTime[i].push(baseURL + '/' + '0'.repeat(1 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '/' + 'IM-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(i + startingTimeIndex, 1)))) + (i + startingTimeIndex) + '-' + '0'.repeat(3 - Math.floor(Math.log10(Math.max(1, i * imgsPerFrame + j + 1)))) + (i * imgsPerFrame + j + 1) + '.' + format + '?raw=true');
-    //         }
-    //     }
-    // } else {
-    //     urlsOverTime.push([]);
-    //     for(let i = 0; i < imgsPerFrame; i++) {
-    //         urlsOverTime[0].push(baseURL + '/' + (1) + '-' + '0'.repeat((Math.floor(Math.log10(imgsPerFrame))) - Math.floor(Math.log10(Math.max(i + startingSpaceIndex, 1)))) + (i + startingSpaceIndex) + '.' + format + '?raw=true');
-    //     }
-    // }
 
     for (let i = 0; i < numFrames; i++) {
         urlsOverTime.push([]);
         for(let j = 0; j < imgsPerFrame; j++) {
-            urlsOverTime[i].push(baseURL + '/' + projection + '/' + '0'.repeat(Math.floor(Math.log10(numFrames)) - Math.floor(Math.log10(Math.max((i + 1), 1)))) + (i  + 1) + '-' + '0'.repeat(Math.floor(Math.log10(imgsPerFrame)) - Math.floor(Math.log10(Math.max(j + 1, 1)))) + (j + 1) + '.' + format);
+            urlsOverTime[i].push(baseURL + '/' + projection + '/' + '0'.repeat(Math.floor(Math.log10(numFrames)) - Math.floor(Math.log10(Math.max((i + 1), 1)))) + (i  + 1) + '-' + '0'.repeat(Math.floor(Math.log10(imgsPerFrame)) - Math.floor(Math.log10(Math.max(j + 1, 1)))) + (j + 1) + '.' + format + '?raw=true');
         }
     }
 
